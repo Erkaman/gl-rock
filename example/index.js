3f978d93f2840d467ce1375325cb8a1dfbf46dbb
Eric Arnebäck <arnebackeric@gmail.com>
2016-05-22 11:19:32 +0200

    now you can randomize and vary many properties.

--- example/index.js
+++ example/index.js
@@ -50,6 +50,28 @@ rockObj = {
     scale:  [1.0, 1.0, 1.0]
 };
 
+const MESH_NOISE_SCALE_MIN = 0.5;
+const MESH_NOISE_SCALE_MAX = 5.0;
+
+const NOISE_STRENGTH_MIN = 0.0;
+const NOISE_STRENGTH_MAX = 1.0;
+
+
+const SCRAPE_COUNT_MIN = 0;
+const SCRAPE_COUNT_MAX = 15;
+
+const SCRAPE_MIN_DIST_MIN = 0.1;
+const SCRAPE_MIN_DIST_MAX = 1.0;
+
+const SCRAPE_STRENGTH_MIN = 0.1;
+const SCRAPE_STRENGTH_MAX = 1.0;
+
+const SCRAPE_RADIUS_MIN = 0.1;
+const SCRAPE_RADIUS_MAX = 1.0;
+
+const SCALE_MIN = +0.0;
+const SCALE_MAX = +2.0;
+
 function newRock(gl) {
 
     rock = new createRock(gl, rockObj );
@@ -67,7 +89,7 @@ shell.on("gl-init", function () {
     paletteDrawer = new PaletteDrawer(gl, [030, 540], [380, 600] );
 
     gui = new createGui(gl);
-    gui.windowSizes = [300, 380];
+    gui.windowSizes = [300, 480];
 
     newRock(gl);
 
@@ -79,6 +101,127 @@ shell.on("gl-init", function () {
 function newSeed() {
     rockObj.seed = Math.round(randomArray(0, 1000000).oned(1)[0]);
 }
+
+
+function randomizeNoise() {
+    rockObj.colorNoiseStrength.val = randomArray(NOISE_STRENGTH_MIN, NOISE_STRENGTH_MAX).oned(1)[0];
+    rockObj.cracksNoiseStrength.val = randomArray(NOISE_STRENGTH_MIN, NOISE_STRENGTH_MAX).oned(1)[0];
+}
+
+
+function varyParameter(param, variance, min, max) {
+
+    param.val  += randomArray(-variance, +variance).oned(1)[0];
+    if(param.val > max) param.val = max;
+    if(param.val < min) param.val = min;
+
+}
+
+function randomizeMesh() {
+
+    rockObj.meshNoiseScale.val = randomArray(MESH_NOISE_SCALE_MIN, MESH_NOISE_SCALE_MAX).oned(1)[0];
+    rockObj.meshNoiseStrength.val = randomArray(NOISE_STRENGTH_MIN, NOISE_STRENGTH_MAX).oned(1)[0];
+
+    rockObj.scrapeCount.val = Math.floor(randomArray(SCRAPE_COUNT_MIN, SCRAPE_COUNT_MAX).oned(1)[0]);
+    rockObj.scrapeMinDist.val = randomArray(SCRAPE_MIN_DIST_MIN, SCRAPE_MIN_DIST_MAX).oned(1)[0];
+
+    rockObj.scrapeStrength.val = randomArray(SCRAPE_STRENGTH_MIN, SCRAPE_STRENGTH_MAX).oned(1)[0];
+
+    rockObj.scrapeRadius.val = randomArray(SCRAPE_RADIUS_MIN, SCRAPE_RADIUS_MAX).oned(1)[0];
+
+    rockObj.scale = randomArray(SCALE_MIN, SCALE_MAX).oned(3);
+}
+
+
+function varyMesh() {
+
+    varyParameter(rockObj.meshNoiseScale, 0.1, MESH_NOISE_SCALE_MIN, MESH_NOISE_SCALE_MAX);
+
+    varyParameter(rockObj.meshNoiseStrength, 0.1, NOISE_STRENGTH_MIN, NOISE_STRENGTH_MAX);
+
+
+    varyParameter(rockObj.scrapeCount, 2, SCRAPE_COUNT_MIN, SCRAPE_COUNT_MAX);
+
+    varyParameter(rockObj.scrapeMinDist, 0.1, SCRAPE_MIN_DIST_MIN, SCRAPE_MIN_DIST_MAX);
+    varyParameter(rockObj.scrapeStrength, 0.1, SCRAPE_STRENGTH_MIN, SCRAPE_STRENGTH_MAX);
+
+    varyParameter(rockObj.scrapeRadius, 0.1, SCRAPE_RADIUS_MIN, SCRAPE_RADIUS_MAX);
+
+
+
+    // vary scale.
+
+    var scale = rockObj.scale;
+
+    var VARY = 0.3;
+
+    scale[0] += randomArray(-VARY, +VARY).oned(1)[0];
+    if(scale[0] > 1.0) scale[0] = 1.0;
+    if(scale[0] < 0.0) scale[0] = 0.0;
+
+
+    scale[1] += randomArray(-VARY, +VARY).oned(1)[0];
+    if(scale[1] > 1.0) scale[1] = 1.0;
+    if(scale[1] < 0.0) scale[1] = 0.0;
+
+
+    scale[2] += randomArray(-VARY, +VARY).oned(1)[0];
+    if(scale[2] > 1.0) scale[2] = 1.0;
+    if(scale[2] < 0.0) scale[2] = 0.0;
+
+    /*
+
+
+
+        rockObj.scrapeRadius.val = randomArray(SCRAPE_RADIUS_MIN, SCRAPE_RADIUS_MAX).oned(1)[0];
+
+        rockObj.scale = randomArray(SCALE_MIN, SCALE_MAX).oned(3);
+    */
+}
+
+
+function varyNoise() {
+
+    varyParameter(rockObj.colorNoiseStrength, 0.1, NOISE_STRENGTH_MIN, NOISE_STRENGTH_MAX);
+    varyParameter(rockObj.cracksNoiseStrength, 0.1, NOISE_STRENGTH_MIN, NOISE_STRENGTH_MAX);
+}
+
+
+function randomizeColor() {
+    rockObj.aColor = randomArray(0, 1).oned(3);
+    rockObj.bColor = randomArray(0, 1).oned(3);
+    rockObj.cColor = randomArray(0, 1).oned(3);
+    rockObj.dColor = randomArray(0, 1).oned(3);
+}
+
+
+function varyColorHelper(color) {
+
+    var VARY = 0.1;
+
+    color[0] += randomArray(-VARY, +VARY).oned(1)[0];
+    if(color[0] > 1.0) color[0] = 1.0;
+    if(color[0] < 0.0) color[0] = 0.0;
+
+
+    color[1] += randomArray(-VARY, +VARY).oned(1)[0];
+    if(color[1] > 1.0) color[1] = 1.0;
+    if(color[1] < 0.0) color[1] = 0.0;
+
+
+    color[2] += randomArray(-VARY, +VARY).oned(1)[0];
+    if(color[2] > 1.0) color[2] = 1.0;
+    if(color[2] < 0.0) color[2] = 0.0;
+}
+
+function varyColor() {
+
+    varyColorHelper(rockObj.aColor);
+    varyColorHelper(rockObj.bColor);
+    varyColorHelper(rockObj.cColor);
+    varyColorHelper(rockObj.dColor);
+}
+
 
 shell.on("gl-render", function (t) {
     var gl = shell.gl
@@ -123,27 +266,7 @@ shell.on("gl-render", function (t) {
 
     gui.separator();
 
-    const MESH_NOISE_SCALE_MIN = 0.5;
-    const MESH_NOISE_SCALE_MAX = 5.0;
 
-    const NOISE_STRENGTH_MIN = 0.0;
-    const NOISE_STRENGTH_MAX = 1.0;
-
-
-    const SCRAPE_COUNT_MIN = 0;
-    const SCRAPE_COUNT_MAX = 15;
-
-    const SCRAPE_MIN_DIST_MIN = 0.1;
-    const SCRAPE_MIN_DIST_MAX = 1.0;
-
-    const SCRAPE_STRENGTH_MIN = 0.1;
-    const SCRAPE_STRENGTH_MAX = 1.0;
-
-    const SCRAPE_RADIUS_MIN = 0.1;
-    const SCRAPE_RADIUS_MAX = 1.0;
-
-    const SCALE_MIN = +0.0;
-    const SCALE_MAX = +2.0;
 
     if(editMode.val == 0) {
         gui.textLine("Mesh");
@@ -161,6 +284,11 @@ shell.on("gl-render", function (t) {
 
         gui.draggerFloat3("Scale", rockObj.scale, [SCALE_MIN, SCALE_MAX], ["X:", "Y:", "Z:"]);
 
+        if (gui.button("Randomize")) { randomizeMesh(); newRock(gl); }
+        gui.sameLine();
+        if (gui.button("Vary")) { varyMesh(); newRock(gl); }
+
+
     } else {
         gui.textLine("Texture");
 
@@ -175,6 +303,11 @@ shell.on("gl-render", function (t) {
         gui.draggerRgb("cColor", rockObj.cColor);
         gui.draggerRgb("dColor", rockObj.dColor);
 
+        if (gui.button("Randomize")) { randomizeColor();newRock(gl);  }
+        gui.sameLine();
+        if (gui.button("Vary")) { varyColor();newRock(gl);  }
+
+
         gui.separator();
 
         gui.textLine("Noise");
@@ -183,9 +316,18 @@ shell.on("gl-render", function (t) {
             rockObj.colorNoiseStrength, NOISE_STRENGTH_MIN, NOISE_STRENGTH_MAX);
         gui.sliderFloat("Cracks Strength",
             rockObj.cracksNoiseStrength, NOISE_STRENGTH_MIN, NOISE_STRENGTH_MAX);
+
+        if (gui.button("Randomize")) { randomizeNoise();newRock(gl);  }
+        gui.sameLine();
+        if (gui.button("Vary")) { varyNoise();newRock(gl);  }
     }
 
     gui.separator();
+
+
+    if (gui.button("Randomize")) { randomizeNoise();randomizeColor(); randomizeMesh();newRock(gl);  }
+    gui.sameLine();
+    if (gui.button("Vary")) { varyNoise();varyColor(); varyMesh(); newRock(gl);  }
 
 
     if (gui.button("New Seed")) {
@@ -194,7 +336,7 @@ shell.on("gl-render", function (t) {
     }
     gui.textLine("Seed: " + rockObj.seed);
 
-    if (gui.button("New Rock")) {
+    if (gui.button("Update Rock")) {
         newRock(gl);
     }
 
